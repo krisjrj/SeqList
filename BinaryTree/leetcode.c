572813e9f36df837f333a8422cb984abe79123ca
Krisjrj <398679378@qq.com>
2021-01-26 22:30:54 +0800

    二叉树

--- BinaryTree/leetcode.c
+++ BinaryTree/leetcode.c
@@ -65,3 +65,17 @@ int numberOfMatches(int n) {
     }
     return i;
 }
+//ÏàÍ¬µÄÊ÷
+bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
+    if (p == NULL && q == NULL)
+        return true;
+    if (p == NULL && q != NULL)
+        return false;
+    if (p != NULL && q == NULL)
+        return false;
+    if (p->val != q->val)
+        return false;
+    else
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+
+}
